5b05da03b27ae3ec66f9eadbfab4a4cdcea98354
Gioh Kim <gi-oh.kim@profitbricks.com>
2016-09-01 17:16:24 +0200

    stack: simple text editor (on going)

---
+++ stack_simple_text_editor.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+/*
+8
+1 abc
+3 3
+2 3
+1 xy
+3 2
+4 
+4 
+3 1
+*/
+
+char string[1000000];
+int strindex;
+
+int main(void)
+{
+	int num_op;
+	int i;
+	int op;
+	int data;
+
+	scanf("%d", &num_op);
+
+	for (i = 0; i < num_op; i++) {
+		scanf("%d", &op);
+
+		switch (op) {
+			char c;
+		case 1:
+			do {
+				scanf(
+			strindex += 
+			break;
+		case 2:
+
+		case 3:
+
+		case 4:
+		}
+	}
+
+	return 0;
+}
