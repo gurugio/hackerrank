75d396c15adc3ed8b2cec7f7448b0c65b0433bff
Gioh Kim <gurugio@hanmail.net>
2016-09-03 20:52:13 +0200

    queue: down to zero2 - fail

--- queue_down_to_zero2.c
+++ queue_down_to_zero2.c
@@ -21,32 +21,35 @@ int go2zero(int num)
 	int i;
 	int *queue;
 	int qindex = 0;
+	int ret;
 
 	if (num == 1)
 		return 1;
 
-	queue = calloc(100, sizeof(int));
+	queue = calloc(10000, sizeof(int));
 
-	for (i = 2; i < (int)sqrt(num); i++) {
+	for (i = 2; i <= (int)sqrt(num); i++) {
 		if ((num % i) == 0) {
-			/* printf("[%d]enq=%d\n", num, (int)num/i); */
+			printf("[%d]enq=%d\n", num, (int)num/i);
 			queue[qindex++] = (int)num/i;
 		}
 	}
 
 	if (qindex == 0) {
 		queue[qindex++] = num - 1;
-		/* printf("[%d]enq=%d\n", num, num-1); */
+		printf("[%d]enq=%d\n", num, num-1);
 	}
 
 	for (i = 0; i < qindex; i++) {
 		int o = queue[i];
 		queue[i] = go2zero(queue[i]);
 		printf("[%d]%d %d\n", num, o, queue[i]);
-	}
+       	}
 
 	// add free q
-	return findmin(queue, qindex) + 1;
+	ret = findmin(queue, qindex) + 1;
+	free(queue);
+	return ret;
 }
 int main(void)
 {
@@ -57,11 +60,19 @@ int main(void)
 
 	/* for (; count > 0; count--) { */
 	/* 	scanf("%d", &num); */
+	/* 	printf("%d\n", go2zero(num)); */
+	/* } */
 
-	/* for (i = 2; i <= 8; i++) */
-	/* 	printf("-->%d--%d\n", i, go2zero(i)); */
+	printf("->%d\n", go2zero(3));
+	printf("->%d\n", go2zero(4));
+	printf("->%d\n", go2zero(5));
+	printf("->%d\n", go2zero(7));
 
 	printf("->%d\n", go2zero(70));
+	printf("->%d\n", go2zero(7273));
+	//printf("->%d\n", go2zero(225604));
+
+
 	/* } */
 	return 0;
 }
