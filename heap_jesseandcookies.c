579f44fe9a6f8b0cff44efa675f161a23b277298
Gioh Kim <gi-oh.kim@profitbricks.com>
2016-09-09 13:10:32 +0200

    heap: jesse and cookies

--- heap_jesseandcookies.c
+++ heap_jesseandcookies.c
@@ -4,12 +4,19 @@
 #define lchild(i) (2*(i) + 1)
 #define rchild(i) (2*(i) + 2)
 
-long heap[100000];
+long heap[1000000];
 int heap_index;
 
 int parent(int i)
 {
 	return (i <= 0) ? -1 : ((int)(((i) - 1)/2));
+}
+
+void swap(long *array, int a, int b)
+{
+	long t = array[a];
+	array[a] = array[b];
+	array[b] = t;
 }
 
 void heap_push(long *array, long val)
@@ -25,19 +32,18 @@ void heap_push(long *array, long val)
 		int t;
 		
 		p = parent(cur);
-		if (p < 0 || array[p] < array[cur])
+		if (p < 0 || array[p] <= array[cur])
 			break;
 
-		t = array[cur];
-		array[cur] = array[p];
-		array[p] = t;
+		swap(array, cur, p);
 		cur = p;
 	}
 
 #ifdef DEBUG
 		int i;
+		printf("after push<%ld>\n", val);
 		for (i = 0; i < heap_index; i++)
-			printf("%ld ", heap[i]);
+			printf("<%d>%ld ", i, heap[i]);
 		printf("\n");
 #endif
 }
@@ -45,51 +51,50 @@ void heap_push(long *array, long val)
 long heap_pop(long *array)
 {
 	int l, r;
-	int cur = 0;
+	int next;
 	long ret = array[0];
+	int p;
 
 	if (heap_index == 0)
 		return -1;
 	
-	array[cur] = array[heap_index - 1];
-	
-	while (cur < heap_index) {
-		l = lchild(cur);
-		r = rchild(cur);
+	heap_index--;
+	array[0] = array[heap_index];
+
+	next = 0;
+	l = r = 0;
+	while (next < heap_index) {
+		p = next;
+		l = lchild(p);
+		r = rchild(p);
 
 #ifdef DEBUG
-		printf("cur=%d l=%d r=%d heap_index=%d\n", cur, l, r, heap_index);
+		printf("p=%d l=%d r=%d heap_index=%d\n", p, l, r, heap_index);
 #endif
 
-		if (l < heap_index && r < heap_index) {
-			int t;
-			if (array[l] < array[r]) {
-				t = array[l];
-				array[l] = array[cur];
-				array[cur] = t;
-				cur = l;
-			} else {
-				t = array[r];
-				array[r] = array[cur];
-				array[cur] = t;
-				cur = r;
-			}
-		} else if (l < heap_index && r >= heap_index) {
-			int t;
-			t = array[l];
-			array[l] = array[cur];
-			array[cur] = t;
-			cur = l;
-		} else
+		if (l >= heap_index && r >= heap_index)
 			break;
+		
+		if (array[p] < array[l] && array[p] < array[r])
+			break;
+		
+		if (l < heap_index && array[l] < array[next])
+			next = l;
+
+		if (r < heap_index && array[r] < array[next])
+			next = r;
+
+		if (next == p)
+			break;
+		swap(array, p, next);
 	}
 
-	heap_index--;
 #ifdef DEBUG
 	{
 		int i;
+		printf("after pop<%ld>\n", ret);
 		for (i = 0; i < heap_index; i++)
-			printf("%ld ", heap[i]);
+			printf("<%d>%ld ", i, heap[i]);
 		printf("\n");
 	}
 #endif
@@ -97,7 +102,7 @@ long heap_pop(long *array)
 	return ret;
 }
 
-int make_sweet(long *array, int least)
+int make_sweet(long *array, long least)
 {
 	long a, b;
 	long min;
@@ -106,18 +111,20 @@ int make_sweet(long *array, int least)
 			int again = 0;
 
 	while (1) {
-		min = heap_pop(array);
+		min = array[0];
 		if (min >= least)
 			break;
-		heap_push(array, min);
 		a = heap_pop(array);
 		b = heap_pop(array);
-
+#ifdef DEBUG
 		printf("a=%ld b=%ld c=%ld\n", a, b, a + 2*b);
-		if (a > 0 && b > 0)
+#endif
+		if (a >= 0 && b >= 0)
 			heap_push(array, a + 2 * b);
 		else 
 			return -1;
+
+		
 		count++;
 	}
 	return count;
@@ -127,18 +134,22 @@ int main(void)
 {
 	int i;
 	int num;
-	int arg;
+	long arg;
 	long val;
 
 	scanf("%d", &num);
-	scanf("%d", &arg);
+	scanf("%ld", &arg);
 
 	for (i = 0; i < num; i++) {
 		scanf("%ld", &val);
 		heap_push(heap, val);
 	}
 
+	/* for (i = 0; i < num; i++) { */
+	/* 	printf("%d %ld\n", i, heap_pop(heap)); */
+	/* } */
+
 	printf("%d\n", make_sweet(heap, arg));
-	
+
 	return 0;
 }
